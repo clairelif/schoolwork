372407b03a0778578f2d5b94f4921b6f7c472216
clairelif <claire4811@tamu.edu>
2025-04-03 03:43:04 -0500

    test

--- MARS102/Lab 9/Lab_9_Ocean_Life.R
+++
@@ -1,5 +0,0 @@
-setwd("C:/Users/clali/OneDrive/Desktop/EOS_CLASS/Lab 9")
-Fauna<-read.csv('fishcatch.csv')
-
-lbls <- paste(Fauna$Species_Name, Fauna$frequency)
-pie(Fauna$frequency, labels = lbls, main="Ocean Life Fauna", col=rainbow(length(lbls)))
--- README.md
+++
@@ -1,7 +0,0 @@
-# schoolwork
-Claire Lifrieri
-Classwork from 2022-2025
-
-| Classes |
-| :------: |
-| [MARS102](schoolwork/MARS102)|
